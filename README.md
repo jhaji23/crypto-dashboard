54c00636d6b70b44806a5dca7f29b96116dbf964
Ajay Jha <ajayha245@gmail.com>
2025-03-25 14:55:38 +0530

    Initial commit: Built Cryptocurrency Dashboard with React, Chart.js, and CoinGecko API

---
+++ README.md
@@ -0,0 +1,28 @@
+# Cryptocurrency Dashboard
+
+This is a cryptocurrency dashboard app built using **React 18**, **Material UI 5**, and **Chart.js 4**. The app uses the **CoinGecko API** to get cryptocurrency data such as live prices, trading volume, exchange volumes, historical data, crypto categories, images, and more.
+
+## Prerequisites
+
+Install the following prerequisites:
+
+1. [Node.js](https://nodejs.org/en/)
+2. [Visual Studio Code](https://code.visualstudio.com/download)
+
+## Installation
+
+#### Install required dependencies
+
+From the **root** directory, run:
+
+```bash
+npm install
+```
+
+## Run the application
+
+From the **root** directory, run:
+
+```bash
+npm start
+```
---
+++ src/components/statistics/AssetPlatforms.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useTheme } from '@mui/material/styles';
+
+// Font Awesome Icon
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChartPie as ChartPieIcon } from '@fortawesome/free-solid-svg-icons';
+library.add(ChartPieIcon);
+
+import CustomCard from '../CustomCard';
+
+const AssetPlatforms = () => {
+  const theme = useTheme();
+
+  const [assetPlatforms, setAssetPlatforms] = useState([]);
+
+  const fetchAssetPlatforms = () => {
+    axios
+      .get('https://api.coingecko.com/api/v3/asset_platforms', {
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+      .then((response) => {
+        setAssetPlatforms(response.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchAssetPlatforms();
+  }, []);
+
+  return (
+    <CustomCard
+      text='ASSET PLATFORMS'
+      value={assetPlatforms.length}
+      color={theme.palette.warning.dark}
+      icon={ChartPieIcon}
+    />
+  );
+};
+
+export default AssetPlatforms;
